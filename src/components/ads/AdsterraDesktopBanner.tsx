b082c32948e114c6704df6b5dfb0cbfac5495540
amrelhaj87-cloud <amr.elhaj87@gmail.com>
2026-08-22 13:24:51 +0400

    Auto-deploy update

--- src/components/ads/AdsterraDesktopBanner.tsx
+++ src/components/ads/AdsterraDesktopBanner.tsx
@@ -6,35 +6,11 @@ import React from 'react';
  * Placement ID: 30861165
  */
 export const AdsterraDesktopBanner: React.FC = () => {
-  const adHtml = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="UTF-8">
-        <style>
-          body, html { margin: 0; padding: 0; overflow: hidden; background: transparent; }
-        </style>
-      </head>
-      <body>
-        <script type="text/javascript">
-          atOptions = {
-            'key' : 'f31a297fcbc2dfc18b704c32b90b84df',
-            'format' : 'iframe',
-            'height' : 600,
-            'width' : 160,
-            'params' : {}
-          };
-        </script>
-        <script type="text/javascript" src="https://www.highperformanceformat.com/f31a297fcbc2dfc18b704c32b90b84df/invoke.js"></script>
-      </body>
-    </html>
-  `;
-
   return (
     <div style={{ width: 160, height: 600, overflow: 'hidden' }}>
       <iframe
         title="Adsterra Desktop Ad"
-        srcDoc={adHtml}
+        src="/ad-desktop.html"
         width="160"
         height="600"
         frameBorder="0"
--- src/components/ads/AdsterraMobileBanner.tsx
+++ src/components/ads/AdsterraMobileBanner.tsx
@@ -6,35 +6,11 @@ import React from 'react';
  * Placement ID: 30861164
  */
 export const AdsterraMobileBanner: React.FC = () => {
-  const adHtml = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="UTF-8">
-        <style>
-          body, html { margin: 0; padding: 0; overflow: hidden; background: transparent; }
-        </style>
-      </head>
-      <body>
-        <script type="text/javascript">
-          atOptions = {
-            'key' : '98cfc8d8c2810931e9c8cb1a4bb66c2c',
-            'format' : 'iframe',
-            'height' : 50,
-            'width' : 320,
-            'params' : {}
-          };
-        </script>
-        <script type="text/javascript" src="https://www.highperformanceformat.com/98cfc8d8c2810931e9c8cb1a4bb66c2c/invoke.js"></script>
-      </body>
-    </html>
-  `;
-
   return (
     <div style={{ width: 320, height: 50, margin: '0 auto', overflow: 'hidden' }}>
       <iframe
         title="Adsterra Mobile Ad"
-        srcDoc={adHtml}
+        src="/ad-mobile.html"
         width="320"
         height="50"
         frameBorder="0"
